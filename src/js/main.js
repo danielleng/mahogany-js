a956ee9b0ebf90c9b31a5d4764635255a8eb4e97
Daniel Leng <danlengjj@gmail.com>
2021-06-07 19:52:50 +0800

    Clean up and prepare for development, add Bootstrap 5.0.1, add Roboto font, add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import $ from './jquery-clone';
+import * as bootstrap from 'bootstrap';
+import $ from './jquery-clone/$clone';
 
-$(document).ready(function() {
-  console.log('Ready.');
-
+function initCarousel() {
   $(document).on('click', '.carousel__navRight', function() {
     const currentImg = $('.active');
     const nextImg = currentImg.next();
@@ -22,15 +21,22 @@ $(document).ready(function() {
       prevImg.addClass('active').css('z-index', 10);
     }
   })
-});
+}
 
-$.get({
-  url: 'https://jsonplaceholder.typicode.com/todos/1',
-  success: data => {
-    console.log('First success', data)
-  }
-})
-  .done(data => console.log('Second success', data))
-  .fail(e => console.error('Fail', e))
-  .always(() => console.log('Always'));
-  
+function testAjax() {
+  $.get({
+    url: 'https://jsonplaceholder.typicode.com/todos/1',
+    success: data => {
+      console.log('First success', data)
+    }
+  })
+    .done(data => console.log('Second success', data))
+    .fail(e => console.error('Fail', e))
+    .always(() => console.log('Always'));
+}
+
+$(document).ready(function() {
+  console.log('Ready.');
+  initCarousel();
+  // testAjax();
+});
